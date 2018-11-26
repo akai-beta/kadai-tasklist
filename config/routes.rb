517db544b34d98f396657c02f5ced31beb3d7836
akai <akai@beta-lab.co>
2018-11-26 13:24:57 +0000

    とりあえず、ユーザーを作って、サインアップされるところまで。flashはつけてない。bootstrapもやってない。裸の状態。

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,21 @@
 Rails.application.routes.draw do
+  get 'users/index'
+
+  get 'users/show'
+
+  get 'users/new'
+
+  get 'users/create'
+
+  get 'toppages/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'tasks#index'
+  #　前回までのルートはtasks@rootだったのをtoppages#indexに変更
+  # root to: 'tasks#index'
+  root to: 'toppages#index'
+  
+  get 'singup', to: 'users#new'
+  resources :users, only: [ :index, :show, :new, :create ]
+  
   resources :tasks
 end
